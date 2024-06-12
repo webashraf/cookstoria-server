17037bdebdb403273f7e500ff021b5571bd04f6e
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-06-12 14:55:42 +0600

    successfully conncet server with mongodb

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,27 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+export default tseslint.config(
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-];
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ['dist/*', '.env', 'node_modules'],
+  },
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'no-undef': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+    },
+  },
+)
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res) => {
+  res.status(500).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello! roducts");
+});
+
+export default app;
