902cbc18713e806cf1fb4da2355db2f97b7862e7
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-06-15 22:38:20 +0600

    Optimieze code for deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import globals from 'globals'
+import eslint from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -13,15 +13,14 @@ export default tseslint.config(
     },
   },
   {
-    ignores: ['dist/*', '.env', 'node_modules'],
-  },
-  {
     rules: {
-      'no-unused-vars': 'error',
-      'no-unused-expressions': 'error',
-      'no-undef': 'error',
-      'prefer-const': 'error',
-      'no-console': 'warn',
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
     },
   },
-)
+  {
+    ignores: ["**/node_modules/", "**/dist/"],
+  }
+);
